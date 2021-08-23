32d7136406d2dc786365e73fec8e65e620154710
Brayan.Granada <brayan.granada.kommit@gmail.com>
2021-08-23 09:15:41 -0500

    add changes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,15 @@
 class UserController < ApplicationController
   # Follows a user.
   def follow
-    Follow.new(follower_id: current_user.id, followed_user_id: params[:id]).save
+    follow = Follow.new(follower_id: current_user.id, followed_user_id: params[:id])
+    follow.save
     redirect_to root_url
   end
 
   # Unfollows a user.
   def unfollow
-    Follow.where(follower_id: current_user.id, followed_user_id: params[:id])[0].destroy
+    follow = Follow.find_by(follower_id: current_user.id, followed_user_id: params[:id])
+    follow.destroy
     redirect_to root_url
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,21 @@
 module ArticlesHelper
+  def show_article?(article)
+    current_user.is_owner?(article) || article.status == 'public'
+  end
+
+  def show_comment?(comment, article)
+    article.user == current_user || comment.status == 'public'
+  end
+
+  def owner_article(article)
+    current_user.is_owner?(article)
+  end
+
+  def owner_comment(comment)
+    comment.user == current_user
+  end
+
+  def is_followings?(article)
+    current_user.followings.include?(article.user)
+  end
 end
